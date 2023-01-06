62f1da54df62e5d1c49736a45533eb0e87f9b29f
zippi-wzp <truebatmandc@gmail.com>
2023-01-06 15:28:11 +0200

    added docker-compose
    added coffee schema
    
    added pagination
    
    added transaction
    
    fixed issue

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -8,6 +8,7 @@ import {
   Delete,
   Query,
 } from '@nestjs/common';
+import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto/pagination-query.dto';
 import { CoffeesService } from './coffees.service';
 import { CreateCoffeeDto } from './dto/create-coffee.dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto/update-coffee.dto';
@@ -17,9 +18,9 @@ export class CoffeesController {
   constructor(private readonly coffeeService: CoffeesService) {}
 
   @Get()
-  findAll(@Query() paginationQuery) {
+  findAll(@Query() paginationQuery: PaginationQueryDto) {
     // const { limit, offset } = paginationQuery;
-    return this.coffeeService.findAll();
+    return this.coffeeService.findAll(paginationQuery);
   }
 
   @Get(':id')
